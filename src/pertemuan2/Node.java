0f118dd07ac999c41fbf017b7129c01d0fde2175
Alyakhairanni <alyakhairani0401@gmail.com>
2025-02-19 21:05:26 +0700

    pertemuan2

---
+++ src/pertemuan2/Node.java
@@ -0,0 +1,24 @@
+package pertemuan2;
+
+public class Node {
+	private final int nilai;
+	private Node next;
+
+	public Node(int nilai) {
+	    this.nilai = nilai;
+	    this.next = null;
+	}
+
+	public int getNilai () {
+		return nilai;
+	}
+	
+	public Node getNext () {
+		return next;
+	}
+	
+	public void setNext (Node next) {
+		this.next = next;
+	}
+}
+	
